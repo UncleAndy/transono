0292f0ea18da2722158f36f9ffbe060a6dc6ffe6
Andrey Velikoredchanin <andy@andyhost.ru>
2026-07-04 19:13:25 +0200

    Simplify provider architecture

--- src/core/provider.rs
+++ src/core/provider.rs
@@ -1,22 +1,10 @@
 use async_trait::async_trait;
-
-use crate::core::provider_event::ProviderEvent;
-use crate::core::provider_command::ProviderCommand;
+// use crate::core::client::Client;
 use crate::core::error::Result;
 
 #[async_trait]
 pub trait Provider {
+    type Error;
 
-    async fn connect(&mut self) -> Result<()>;
-
-    async fn disconnect(&mut self) -> Result<()>;
-
-    async fn send(
-        &mut self,
-        command: ProviderCommand,
-    ) -> Result<()>;
-
-    async fn next_event(
-        &mut self,
-    ) -> Result<ProviderEvent>;
+    // async fn connect(&self) -> Result<Client<>>;
 }
