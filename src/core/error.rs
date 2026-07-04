56ccd6e8438408fed766d245d39964129fd23bde
Andrey Velikoredchanin <andy@andyhost.ru>
2026-07-04 11:37:40 +0200

    Fix core/error.

--- src/core/error.rs
+++ src/core/error.rs
@@ -6,10 +6,18 @@ std::result::Result<T, CoreError>;
 #[derive(Debug, Error)]
 pub enum CoreError {
 
-    #[error(transparent)]
-    Other(
-        #[from]
-        anyhow::Error,
-    ),
+    #[error("transport error")]
+    Transport(#[from] TransportError),
 
+    #[error("protocol error")]
+    Protocol(#[from] ProtocolError),
+
+    #[error(transparent)]
+    Other(#[from] anyhow::Error),
 }
+
+#[derive(Debug, Error)]
+pub enum TransportError {}
+
+#[derive(Debug, Error)]
+pub enum ProtocolError {}
