1394be92d64a35273cf2a3e63afdc7e0dc7a4904
Andrey Velikoredchanin <andy@andyhost.ru>
2026-07-04 10:36:04 +0200

    Small refact.

--- src/core/transport.rs
+++ src/core/transport.rs
@@ -20,7 +20,43 @@ pub trait Transport: Send + Sync {
     ) -> Result<()>;
 }
 
+#[derive(Debug, Clone)]
 pub enum TransportData {
     Text(String),
     Binary(Vec<u8>),
+}
+
+
+impl TransportData {
+    pub fn as_bytes(&self) -> &[u8] {
+        match self {
+            Self::Text(text) => text.as_bytes(),
+            Self::Binary(data) => data.as_slice(),
+        }
+    }
+
+    pub fn into_bytes(self) -> Vec<u8> {
+        match self {
+            Self::Text(text) => text.into_bytes(),
+            Self::Binary(data) => data,
+        }
+    }
+}
+
+impl From<String> for TransportData {
+    fn from(value: String) -> Self {
+        Self::Text(value)
+    }
+}
+
+impl From<&str> for TransportData {
+    fn from(value: &str) -> Self {
+        Self::Text(value.to_owned())
+    }
+}
+
+impl From<Vec<u8>> for TransportData {
+    fn from(value: Vec<u8>) -> Self {
+        Self::Binary(value)
+    }
 }
