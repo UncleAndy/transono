07975f43418a3a331dfc5067fb81342cc8061d02
Andrey Velikoredchanin <andy@andyhost.ru>
2026-07-07 12:58:50 +0200

    checkpoint

--- src/audio/encoders/base64.rs
+++ src/audio/encoders/base64.rs
@@ -1,21 +1,22 @@
 use crate::core::error::Result;
 use crate::audio::{AudioDecoder, AudioEncoder, EncodedAudio, EncodedAudioFormat, PcmAudio};
+use crate::audio::encoders::{PcmBinaryDecoder, PcmBinaryEncoder};
 
 pub struct PcmBase64Encoder {
-    format: EncodedAudioFormat
+    binary: PcmBinaryEncoder,
 }
 
 impl PcmBase64Encoder {
     pub(crate) fn new(format: &EncodedAudioFormat) -> Self {
         Self {
-            format: format.clone()
+            binary: PcmBinaryEncoder::new(format)
         }
     }
 }
 
 impl AudioEncoder for PcmBase64Encoder {
     fn format(&self) -> &EncodedAudioFormat {
-        &self.format
+        &self.binary.format()
     }
 
     fn encode(&self, audio: &PcmAudio) -> Result<EncodedAudio> {
@@ -24,20 +25,20 @@ impl AudioEncoder for PcmBase64Encoder {
 }
 
 pub struct PcmBase64Decoder {
-    format: EncodedAudioFormat
+    binary: PcmBinaryDecoder
 }
 
 impl PcmBase64Decoder {
     pub(crate) fn new(format: &EncodedAudioFormat) -> Self {
         Self {
-            format: format.clone()
+            binary: PcmBinaryDecoder::new(format)
         }
     }
 }
 
 impl AudioDecoder for PcmBase64Decoder {
     fn format(&self) -> &EncodedAudioFormat {
-        &self.format
+        &self.binary.format()
     }
 
     fn decode(&self, encoded: &EncodedAudio) -> Result<PcmAudio> {
