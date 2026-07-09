baf1877db46a354c25f7e3cac188ecbfc29acf1f
Andrey Velikoredchanin <andy@andyhost.ru>
2026-07-09 19:27:08 +0200

    checkpoint

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-use anyhow::Result;
-use cpal::traits::{DeviceTrait};
+use anyhow::{anyhow, Result};
+use cpal::traits::{DeviceTrait, HostTrait};
 use symphonia::core::audio::{AudioSpec, Channels, Position};
 use tokio::signal;
-
 use realtime_translator::audio::{AudioDevicesCpal, AudioInput, AudioInputCpal, AudioOutput, AudioOutputCpal, Processor};
 use realtime_translator::audio::processors::channel_converter::ChannelConverter;
 use realtime_translator::audio::processors::resampler::Resampler;
@@ -10,7 +9,8 @@ use realtime_translator::providers::openai::translation::{
     OpenAITranslationConfig,
     OpenAITranslationProvider,
 };
-
+use std::process::Command;
+use std::time::Duration;
 use realtime_translator::runtime::TranslationLine;
 
 /*
@@ -42,6 +42,27 @@ async fn main() -> Result<()> {
 
     let capture = devices.default_input()?;
     let playback = devices.default_output()?;
+
+
+    let virtual_devices = VirtualDevices::create("EN");
+
+    tokio::time::sleep(Duration::from_secs(1)).await;
+
+    let host = devices.host();
+
+    let virtual_speaker = host
+        .output_devices()?
+        .find(|d| {
+            d.description().unwrap().name().contains("translator_en_speaker")
+        })
+        .expect("virtual speaker not found");
+
+    let virtual_microphone = host
+        .input_devices()?
+        .find(|d| {
+            d.description().unwrap().name().contains("translator_en_microphone")
+        })
+        .expect("virtual microphone not found");
 
     println!("Translator Example");
     println!("===========================");
@@ -147,6 +168,112 @@ async fn main() -> Result<()> {
     line.stop().await?;
 
     println!("Done.");
+
+    Ok(())
+}
+
+
+pub struct VirtualDevices {
+    pub sink_name: String,
+    pub source_name: String,
+
+    sink_module: u32,
+    source_module: u32,
+}
+
+impl VirtualDevices {
+    pub fn create(lang: &str) -> Result<Self> {
+        let lang = lang.to_lowercase();
+
+        let sink_name = format!("translator_{lang}_speaker");
+        let source_name = format!("translator_{lang}_microphone");
+
+        //
+        // Virtual speaker
+        //
+        let sink_module = load_module(
+            "module-null-sink",
+            &[
+                ("sink_name", &sink_name),
+                (
+                    "sink_properties",
+                    &format!(
+                        "device.description=Translator.{}.Speaker",
+                        lang.to_uppercase()
+                    ),
+                ),
+            ],
+        )?;
+
+        //
+        // Virtual microphone
+        //
+        let source_module = load_module(
+            "module-remap-source",
+            &[
+                ("master", &format!("{sink_name}.monitor")),
+                ("source_name", &source_name),
+                (
+                    "source_properties",
+                    &format!(
+                        "device.description=Translator.{}.Microphone",
+                        lang.to_uppercase()
+                    ),
+                ),
+            ],
+        )?;
+
+        Ok(Self {
+            sink_name,
+            source_name,
+            sink_module,
+            source_module,
+        })
+    }
+}
+
+impl Drop for VirtualDevices {
+    fn drop(&mut self) {
+        let _ = unload_module(self.source_module);
+        let _ = unload_module(self.sink_module);
+    }
+}
+
+fn load_module(
+    module: &str,
+    args: &[(&str, &str)],
+) -> Result<u32> {
+    let mut cmd = Command::new("pactl");
+
+    cmd.arg("load-module");
+    cmd.arg(module);
+
+    for (k, v) in args {
+        cmd.arg(format!("{k}={v}"));
+    }
+
+    let out = cmd.output()?;
+
+    if !out.status.success() {
+        return Err(anyhow!(
+            "{}",
+            String::from_utf8_lossy(&out.stderr)
+        ));
+    }
+
+    Ok(String::from_utf8(out.stdout)?
+        .trim()
+        .parse()?)
+}
+
+fn unload_module(id: u32) -> Result<()> {
+    let status = Command::new("pactl")
+        .args(["unload-module", &id.to_string()])
+        .status()?;
+
+    if !status.success() {
+        return Err(anyhow!("pactl unload-module failed"));
+    }
 
     Ok(())
 }
