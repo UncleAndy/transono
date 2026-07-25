310e0c5a7f00722c439e26e86c9a374bf06cdfb2
Andrey Velikoredchanin <andy@andyhost.world>
2026-07-25 04:49:39 +0200

    Refactoring for AudioSplitter.

--- src/runtime/splitter.rs
+++ src/runtime/splitter.rs
@@ -1,4 +1,5 @@
 use tokio_util::sync::CancellationToken;
+use futures_util::StreamExt;
 use crate::audio::{AudioFormat, AudioInput};
 use crate::runtime::AudioLink;
 use crate::runtime::receiver_port::ReceiverPort;
@@ -54,6 +55,81 @@ impl AudioSplitter {
     #[allow(unused)]
     /// Starts the splitter's processing loop in a background task.
     pub fn start(&mut self) {
-        todo!()
+        let cancel = self.cancel.clone();
+        let senders: Vec<_> = self.outputs.iter().map(|o| o.sender()).collect();
+
+        let stream_result = self.input.stream();
+        let start_result = self.input.start();
+
+        if let (Ok(mut stream), Ok(())) = (stream_result, start_result) {
+            tokio::spawn(async move {
+                loop {
+                    tokio::select! {
+                        _ = cancel.cancelled() => {
+                            break;
+                        }
+                        opt_audio = stream.next() => {
+                            let Some(audio) = opt_audio else {
+                                break;
+                            };
+                            for tx in &senders {
+                                let _ = tx.send(audio.clone()).await;
+                            }
+                        }
+                    }
+                }
+            });
+        }
+    }
+
+    /// Stops the splitter's processing loop.
+    pub fn stop(&self) {
+        self.cancel.cancel();
+    }
+}
+
+impl Drop for AudioSplitter {
+    fn drop(&mut self) {
+        self.cancel.cancel();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::audio::{Audio, EncodedAudioFormat, PcmAudio};
+    use symphonia::core::audio::AudioSpec;
+
+    #[tokio::test]
+    async fn test_audio_splitter() {
+        let format = AudioFormat::from(EncodedAudioFormat::internal_format());
+        let (tx_port, rx_port) = AudioLink::new_ports(format.clone(), 10);
+
+        let mut splitter = AudioSplitter::new(format.clone(), 10, Box::new(rx_port));
+        let mut out1 = splitter.create_output();
+        let mut out2 = splitter.create_output();
+
+        splitter.start();
+
+        let pcm = PcmAudio::new(
+            AudioSpec::new(format.sample_rate, symphonia::core::audio::Channels::Discrete(format.channels)),
+            10,
+        );
+        let audio = Audio::from_pcm(&pcm).unwrap();
+
+        tx_port.sender().send(audio.clone()).await.unwrap();
+
+        let mut stream1 = out1.stream().unwrap();
+        let mut stream2 = out2.stream().unwrap();
+
+        let received1 = tokio::time::timeout(std::time::Duration::from_secs(1), stream1.next()).await;
+        let received2 = tokio::time::timeout(std::time::Duration::from_secs(1), stream2.next()).await;
+
+        assert!(received1.is_ok());
+        assert!(received2.is_ok());
+        assert!(received1.unwrap().is_some());
+        assert!(received2.unwrap().is_some());
+
+        splitter.stop();
     }
 }
